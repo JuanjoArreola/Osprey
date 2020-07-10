beee5e6916da3479f30484334802adea1d40b841
Juanjo <juanjo.arreola@gmail.com>
2020-07-10 14:10:44 -0500

    QueryParameters

---
+++ Sources/Osprey/QueryParameters.swift
@@ -0,0 +1,32 @@
+//
+//  QueryParameters.swift
+//  Osprey
+//
+//  Created by JuanJo on 10/07/20.
+//
+
+import Foundation
+
+public struct QueryParameters: URLQueryStringConvertible {
+    private var array = [URLQueryStringConvertible]()
+    
+    public init(_ elements: URLQueryStringConvertible?...) {
+        array.append(contentsOf: elements.compactMap({ $0 }))
+    }
+    
+    public mutating func add(_ element: URLQueryStringConvertible?) {
+        if let element = element {
+            array.append(element)
+        }
+    }
+    
+    public mutating func add(_ key: String, value: CustomStringConvertible?) {
+        if let value = value {
+            array.append([key: value])
+        }
+    }
+    
+    public var urlQueryString: String? {
+        return array.compactMap({ $0.urlQueryString }).joined(separator: "&")
+    }
+}
