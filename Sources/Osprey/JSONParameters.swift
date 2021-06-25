a5c7413a23f21f17c51180d66dd40545d1e04758
Juanjo <juanjo.arreola@gmail.com>
2021-06-24 20:16:06 -0500

    JSONParameters  doesn't inherit from URLParameters

--- Sources/Osprey/JSONParameters.swift
+++ Sources/Osprey/JSONParameters.swift
@@ -7,22 +7,30 @@
 
 import Foundation
 
-open class JSONParameters: URLParameters {
+open class JSONParameters: RequestParameters {
     public static let encoder = JSONEncoder()
     
     // MARK: -
+    public var urlParameters: URLQueryConvertible? = nil
+    public var requiresAuthentication: Bool = false
+    public var headers: [String: String] = [:]
+    
+    // MARK: - Body
     private var dataClosure: (() throws -> Data)?
     public var data: Data?
     
-    public convenience init<T: Encodable>(_ body: T, headers: [String: String] = [:], authenticate: Bool = false) {
-        self.init(headers: headers)
-        
-        self.dataClosure = { return try JSONParameters.encoder.encode(body) }
+    public init(headers: [String: String] = [:], authenticate: Bool = false) {
+        self.headers = headers
         self.headers["Content-Type"] = "application/json"
         self.requiresAuthentication = authenticate
     }
     
-    public override func getData() throws -> Data? {
+    public convenience init<T: Encodable>(_ body: T, headers: [String: String] = [:], authenticate: Bool = false) {
+        self.init(headers: headers, authenticate: authenticate)
+        self.dataClosure = { return try JSONParameters.encoder.encode(body) }
+    }
+    
+    public func getData() throws -> Data? {
         if data == nil {
             data = try dataClosure?()
         }
